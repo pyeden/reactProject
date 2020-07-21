7da5745fbbb9422f910c018f7acf633b6736c96e
“dengs” <1019217919@qq.com>
2020-07-21 23:05:30 +0800

    sass全局性css配置

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -487,7 +487,14 @@ module.exports = function(webpackEnv) {
                   sourceMap: isEnvProduction && shouldUseSourceMap,
                 },
                 'sass-loader'
-              ),
+              ).concat({
+                  loader: 'sass-resources-loader',
+                  options: {
+                    resources: [
+                      path.resolve(__dirname, './../src/styles/main.scss')
+                    ]
+                  }
+              }),
               // Don't consider CSS imports dead code even if the
               // containing package claims to have no side effects.
               // Remove this when webpack adds a warning or an error for this.
