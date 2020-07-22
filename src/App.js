2473848f4eda948ac3fbbe4f942ee375ca19d368
“dengs” <1019217919@qq.com>
2020-07-23 00:18:17 +0800

    结构精简

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import { Button } from 'antd';
-import './App.scss';
 import Home from './views/Home';
 import Info from './views/info';
 
