30133fc47f6916bad8d8f277342ed4e450d6363c
“dengs” <1019217919@qq.com>
2020-07-25 23:15:13 +0800

    使用环境变量，es6语法练习

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,11 +3,14 @@ const { createProxyMiddleware} = require('http-proxy-middleware')
 
 module.exports = function(app) {
 
-    app.use(createProxyMiddleware('/devApi',{
-        target:"http://localhost:5000/v1",
+    app.use(createProxyMiddleware([process.env.REACT_APP_API_PROXY_URL],{
+        target: process.env.REACT_APP_BACKEND_URL,
         changeOrigin:true,
         pathRewrite: {
-            "^/devApi": "",
+            //ES6语法
+            //反引号引用字符串，换行不需要加号连接
+            // 引入字符串，${变量名}，前后不需要加号连接
+            [`^${process.env.REACT_APP_API_PROXY_URL}`]: "",   
         }
     }))
 
--- src/utils/request.js
+++ src/utils/request.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const service = axios.create({
-    baseURL: "devApi",
+    baseURL: process.env.REACT_APP_API_BASE_URL,
     timeout:5000,
 })
 
